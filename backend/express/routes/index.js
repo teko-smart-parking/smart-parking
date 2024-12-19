b51335b96ae56e90e2bc9bde6d281c6d305fe005
Pascal <75321417+D4rkDeemon@users.noreply.github.com>
2024-12-19 10:42:50 +0100

    Add status message

--- backend/express/routes/index.js
+++ backend/express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SMARP Backend' });
+  res.render('index', { title: 'SMARP API' });
 });
 
 module.exports = router;
