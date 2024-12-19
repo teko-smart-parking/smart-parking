ce526aa078871fd5d591645848e541333e668d9b
Pascal <75321417+D4rkDeemon@users.noreply.github.com>
2024-12-19 11:37:07 +0100

    Some basic routing

--- backend/express/routes/api/index.js
+++ backend/express/routes/api/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('api/index', { title: 'SMARP API - Overview' });
 });
 
 module.exports = router;
